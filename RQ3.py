e8f83d422b32d4b48e947533476344e562a0b365
myz12138 <57956561+myz12138@users.noreply.github.com>
2024-12-10 20:08:32 +0800

    Update RQ3.py

--- RQ3.py
+++ RQ3.py
@@ -6,8 +6,8 @@ from openai import OpenAI
 # from openai import OpenAI
 # from openai import OpenAIError
 import time
-MAX_RETRIES = 5  # 最大重试次数
-RETRY_DELAY = 10  # 重试延迟时间(秒)
+MAX_RETRIES = 5  
+RETRY_DELAY = 10  
 
 
 def read_json(jsonfile_path):
@@ -20,8 +20,8 @@ def read_json(jsonfile_path):
     return a
 task='reachability'
 data_type='medium'
-model_name="gpt-4o"#'gpt-4o''claude-3-sonnet-20240229'
-data_file='100_case_data'
+model_name="gpt-4o"#'gpt-4o'
+data_file='case_data'
 jsonfile_path='./'+data_file+'/'+task+'/'+data_type+'/countnodes_of_'+task+'_'+model_name+'.json'
 json_file=read_json(jsonfile_path)
 initial,new=0,0
@@ -29,4 +29,3 @@ for key,value in json_file.items():
     print(value['initial_judge'])
     initial+=int(value['initial_judge'])
     new+=int(value['new_judge'])
-print(initial/100,new/100)
