7139d07ac00000de39df5b9f2283da910a8fcc4f
myz12138 <57956561+myz12138@users.noreply.github.com>
2024-12-10 20:14:44 +0800

    Update data_distribution.py

--- data_distribution.py
+++ data_distribution.py
@@ -1,11 +1,8 @@
 import json
 task='reachability'
 data_type='medium'
-model_name="gpt-4o"
-#edge_existence
-#reachability
-#cycle_check
-#计算答案分布情况
+model_name=""
+
 with open('./new_code/data/'+task+'/'+data_type+'/'+task+'_examples.json','r') as load_f:
     load_dict = json.load(load_f)
     k1=0
@@ -21,7 +18,7 @@ with open('./new_code/data/'+task+'/'+data_type+'/'+task+'_examples.json','r') a
             k1+=1
     print(k1/len(load_dict))
     print(k2/len(load_dict))
-    a=1
+
 
 
 
